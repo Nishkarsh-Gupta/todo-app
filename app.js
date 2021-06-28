479e29ba05263f003cebf9e256a4907636375d5c
Nishkarsh <nishkarsh.gupta1036@gmail.com>
2021-06-28 12:08:00 +0530

    updated

--- app.js
+++ app.js
@@ -134,6 +134,6 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("server is up");
 });
